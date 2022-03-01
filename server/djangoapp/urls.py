96a18ecbdc8ef8221cf84eed5dad88d778a5f414
raja d <rajaone987@gmail.com>
2022-03-01 16:38:37 +0000

    week 2 update

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -16,10 +16,15 @@ urlpatterns = [
     path(route='contact/', view=views.contact_request, name='contact'),
 
     # path for registration
+    path('registration/', views.registration_request, name='registration'),
 
     # path for login
+    path('login/', views.login_request, name='login'),
 
     # path for logout
+    path('logout/', views.logout_request, name='logout'),
+
+    #path(route='', view=views.get_dealerships, name='index'),
 
     path(route='', view=views.get_dealerships, name='index'),
 
